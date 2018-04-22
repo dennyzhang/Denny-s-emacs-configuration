bd9b2cb6b83796e77a1404b0deffb6b8f7b28b67
DennyZhang <contact@dennyzhang.com>
2018-04-22 14:00:21 -0500

    Daily git commit

--- org-publish/org-publish-to-wordpress.el
+++ org-publish/org-publish-to-wordpress.el
@@ -4,7 +4,7 @@
 ;; Author: Denny Zhang(https://www.dennyzhang.com/contact)
 ;; Copyright 2015, https://DennyZhang.com
 ;; Created:2008-10-01
-;; Updated: Time-stamp: <2018-04-21 07:47:49>
+;; Updated: Time-stamp: <2018-04-22 09:15:11>
 ;;
 ;; --8<-------------------------- separator ------------------------>8--
 (setq denny-linkedin-url "https://www.linkedin.com/in/dennyzhang001")
@@ -341,6 +341,9 @@ the plist used as a communication channel."
 
     (goto-char (point-min))
     (while (re-search-forward "。" nil t) (replace-match "."))
+
+    (goto-char (point-min))
+    (while (re-search-forward "；" nil t) (replace-match ";"))
 
     (goto-char (point-min))
     (while (re-search-forward "—" nil t) (replace-match "-"))
